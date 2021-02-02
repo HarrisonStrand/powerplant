fefc32673e113f8e5887518c1d6cc7f3be7d9657
HarrisonStrand <harrisonstrand@gmail.com>
2021-02-02 11:02:21 -0800

    add state change function to plant
    Co-authored-by: David Sterry <sterry.david@gmail.com>

--- src/main.js
+++ src/main.js
@@ -2,14 +2,9 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import TODO from './services/TODO.js';
+import plant from './src/plant.js';
 
 //BUSINESS LOGIC//
 
 
 //USER INTERFACE LOGIC//
-
-$(document).ready(function(event) {
-  event.preventDefault();
-
-});
