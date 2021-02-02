aee8e250a514571630028f438585fa69fb4124ca
HarrisonStrand <harrisonstrand@gmail.com>
2021-02-02 13:58:39 -0800

    add functionality for palm tree
    Co-authored-by: David Sterry <sterry.david@gmail.com>

--- src/js/plant.js
+++ src/js/plant.js
@@ -4,18 +4,21 @@ import './../css/styles.css';
 
 // This function stores our state.
 
-const storeState = () => {
-	let currentState = {};
+const storeState = (intialState) => {
+	let currentState = intialState;
 	return (stateChangeFunction = state => state) => {
 		const newState = stateChangeFunction(currentState);
 		currentState = {...newState};
 		return newState;
 	}
-  }
-  
+	}
+	
   const stateControl = storeState();
-  const pineTreeControl = stateControl({ soil: 5, water: 2 });
-//   const pineTree = pineTreeControl();
+  const pineTreeControl = storeState({soil: 5, water: 2});
+  const palmTreeControl = storeState({soil: 12, water: 7});
+	let intialState = { pineTreeControl, palmTreeControl };
+	
+	
   // This is a function factory. We can easily create more specific functions that alter a plant's soil, water, and light to varying degrees.
   
   const changeState = (prop) => {
@@ -60,6 +63,8 @@ const storeState = () => {
 			const newState = pineTreeControl(superWater);
 			$('#hydration-value').text(`${newState.water}`);
 		});
+		
+		
 
 		
 		// This function doesn't actually do anything useful in this application - it just demonstrates how we can "look" at the current state (which the DOM is holding anyway). However, students often do need the ability to see the current state without changing it so it's included here for reference.
@@ -69,5 +74,12 @@ const storeState = () => {
 			const currentState = pineTreeControl();
 			$('#soil-value').text(`${currentState.soil}`);
 			$('#hydration-value').text(`${currentState.water}`);
+    });
+        
+		$('#show-state2').click(function() {
+			// We just need to call stateControl() without arguments to see our current state.
+			const currentState = palmTreeControl();
+			$('#soil-value2').text(`${currentState.soil}`);
+			$('#hydration-value2').text(`${currentState.water}`);
 		});
-		});
+	});
